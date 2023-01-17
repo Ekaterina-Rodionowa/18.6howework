97268875e1d57a45944c3ae74179467fea4a3dcf
Ekaterina Rodionova <katyusha2109@yandex.ru>
2023-01-17 22:51:28 +0300

    Initial Commit

---
+++ 18.6Homework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  18.6Homework
+//
+//  Created by Катерина Родионова on 17.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
